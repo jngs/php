73c88960b33e163972d6315a8535ec80304de56e
Jan Langer <jan.langer@goodshape.cz>
2015-01-12 14:42:51 +0100

    use PSR-0 autoloader; bump dev requirment

--- src/loader.php
+++ src/loader.php
@@ -1,16 +1,19 @@
 <?php
 
-require_once __DIR__ . '/Consumerr/Entities/Error.php';
-require_once __DIR__ . '/Consumerr/Entities/Event.php';
-require_once __DIR__ . '/Consumerr/Entities/Part.php';
-require_once __DIR__ . '/Consumerr/Entities/Access.php';
-
-require_once __DIR__ . '/Consumerr/Sender/ISender.php';
-require_once __DIR__ . '/Consumerr/Sender/PhpSender.php';
-require_once __DIR__ . '/Consumerr/Sender/CurlSender.php';
-
-require_once __DIR__ . '/Consumerr/Configuration.php';
-require_once __DIR__ . '/Consumerr/DebugLogger.php';
-require_once __DIR__ . '/Consumerr/Consumerr.php';
-
 require_once __DIR__ . '/Consumerr/exceptions.php';
+
+function consumerr_autoload($className)
+{
+	$className = ltrim($className, '\\');
+	$fileName  = '';
+
+	if ($lastNsPos = strrpos($className, '\\')) {
+		$namespace = substr($className, 0, $lastNsPos);
+		$className = substr($className, $lastNsPos + 1);
+		$fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+	}
+	$fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+
+	require __DIR__.'/'.$fileName;
+}
+spl_autoload_register('consumerr_autoload');
